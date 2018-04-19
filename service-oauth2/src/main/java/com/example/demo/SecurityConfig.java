03281d663b18ebab3de3dbc69c3ded243193ad11
liujianzhong <liujianzhong@baidu.com>
2018-04-19 20:37:19 +0800

    spring cloud1.5 oauth:code

---
+++ service-oauth2/src/main/java/com/example/demo/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by Jason on 18/4/19.
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin().loginPage("/login").permitAll().and().authorizeRequests()
+                .anyRequest().authenticated();
+    }
+}
